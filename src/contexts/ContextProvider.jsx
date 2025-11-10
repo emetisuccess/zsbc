7037d8d13799ca55869202ddae5ca91892402c75
Emetisuccess <emetisuccess@gmail.com>
2025-11-10 15:19:14 +0100

    add file modification

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -1,38 +1,34 @@
 import { createContext, useContext, useState } from "react";
 
 const StateContext = createContext({
-    user: null,
-    token: null,
-    setUser: () => { },
-    setToken: () => { }
+	user: null,
+	token: null,
+	setUser: () => {},
+	setToken: () => {},
 });
 
 export const ContextProvider = ({ children }) => {
-    const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
-    const [user, _setUser] = useState(localStorage.getItem("AUTH_USER"));
+	const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
+	const [user, _setUser] = useState(localStorage.getItem("AUTH_USER"));
 
-    const setToken = (token) => {
-        _setToken(token);
-        if (token) {
-            localStorage.setItem("ACCESS_TOKEN", token);
-        } else {
-            localStorage.removeItem("ACCESS_TOKEN");
-        }
-    }
+	const setToken = (token) => {
+		_setToken(token);
+		if (token) {
+			localStorage.setItem("ACCESS_TOKEN", token);
+		} else {
+			localStorage.removeItem("ACCESS_TOKEN");
+		}
+	};
 
-    const setUser = (user) => {
-        _setUser(user);
-        if (user) {
-            localStorage.setItem("AUTH_USER", JSON.stringify(user));
-        } else {
-            localStorage.removeItem("AUTH_USER");
-        }
-    }
+	const setUser = (user) => {
+		_setUser(user);
+		if (user) {
+			localStorage.setItem("AUTH_USER", JSON.stringify(user));
+		} else {
+			localStorage.removeItem("AUTH_USER");
+		}
+	};
 
-    return (
-        <StateContext value={{ user, token, setUser, setToken }}>
-            {children}
-        </StateContext>
-    )
-}
+	return <StateContext value={{ user, token, setUser, setToken }}>{children}</StateContext>;
+};
 export const useStateContext = () => useContext(StateContext);
